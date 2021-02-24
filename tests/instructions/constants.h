34cd527815e400d6daac69d45455e46215e51519
sylvaus <pierreyves.breches74@gmail.com>
2021-02-23 21:13:49 -0500

    Added more test for INC instruction

--- tests/instructions/constants.h
+++ tests/instructions/constants.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <string>
 #include <vector>
 #include <unordered_map>
 #include <functional>
+
+#include "gtest/gtest.h"
+
 #include "emulator/memory/registers.h"
 
 
@@ -13,7 +17,11 @@ namespace emulator::instructions
     using std::function;
     using emulator::memory::Registers;
 
-    const unordered_map<uint16_t, function<void(Registers&, uint8_t value)>> REGISTER_8_BITS_VALUE_REFERENCE_MAP = {
+    const auto REGISTER_8_BITS_VALUES = ::testing::Values(
+        0b111, 0b000, 0b001, 0b010, 0b011, 0b100, 0b101
+    );
+    // Table Chapter 4: section 2.1 in GameBoyProgManVer1.1.pdf  symbol: r
+    const unordered_map<uint16_t, function<void(Registers&, uint8_t value)>> REGISTER_8_BITS_VALUE_SETTER_MAP = {
         {0b111, [](Registers& registers, uint8_t value){registers.A = value;}},
         {0b000, [](Registers& registers, uint8_t value){registers.B= value;}},
         {0b001, [](Registers& registers, uint8_t value){registers.C = value;}},
@@ -22,11 +30,44 @@ namespace emulator::instructions
         {0b100, [](Registers& registers, uint8_t value){registers.H = value;}},
         {0b101, [](Registers& registers, uint8_t value){registers.L = value;}}
     };
+    const unordered_map<uint16_t, std::string> REGISTER_8_BITS_VALUE_NAME_MAP = {
+        {0b111, "Register_A"},
+        {0b000, "Register_B"},
+        {0b001, "Register_C"},
+        {0b010, "Register_D"},
+        {0b011, "Register_E"},
+        {0b100, "Register_H"},
+        {0b101, "Register_L"}
+    };
 
+    const auto REGISTER_16_BITS_VALUES = ::testing::Values(0b00, 0b01, 0b10, 0b11);
+    // Table Chapter 4: section 2.4 in GameBoyProgManVer1.1.pdf symbol: ss
     const unordered_map<uint16_t, function<void(Registers&, uint16_t value)>> REGISTER_16_BITS_VALUE_SETTER_MAP = {
         {0b00, [](Registers& registers, uint16_t value){registers.set_BC(value);}},
         {0b01, [](Registers& registers, uint16_t value){registers.set_DE(value);}},
         {0b10, [](Registers& registers, uint16_t value){registers.set_HL(value);}},
         {0b11, [](Registers& registers, uint16_t value){registers.SP = value;}}
     };
+    const unordered_map<uint16_t, std::string> REGISTER_16_BITS_VALUE_NAME_MAP = {
+        {0b00, "Register_BC"},
+        {0b01, "Register_DE"},
+        {0b10, "Register_HL"},
+        {0b11, "Register_SP"}
+    };
+
+
+    template<class ParamType>
+    class NameMapPrinter
+    {
+    public:
+        explicit NameMapPrinter(const std::unordered_map<ParamType, std::string>& name_map): name_map_(name_map) {}
+        std::string operator()(const testing::TestParamInfo<ParamType>& info) const
+        {
+            return name_map_.at(info.param);
+        }
+
+    private:
+        const std::unordered_map<ParamType, std::string>& name_map_;
+    };
+
 }
--- tests/instructions/test_inc.cpp
+++ tests/instructions/test_inc.cpp
@@ -14,14 +14,20 @@ The pages referenced in this file are pointing to [GameBoyProgManVer1.1.pdf](htt
 
 using emulator::generated::Arguments;
 using emulator::memory::Registers;
+using emulator::instructions::REGISTER_8_BITS_VALUES;
+using emulator::instructions::REGISTER_16_BITS_VALUES;
+using emulator::instructions::REGISTER_8_BITS_VALUE_SETTER_MAP;
+using emulator::instructions::REGISTER_8_BITS_VALUE_NAME_MAP;
 using emulator::instructions::REGISTER_16_BITS_VALUE_SETTER_MAP;
+using emulator::instructions::REGISTER_16_BITS_VALUE_NAME_MAP;
+using emulator::instructions::NameMapPrinter;
 using emulator::instructions::fixtures::InstructionTestFixture;
 
 namespace gen = emulator::generated;
 
 namespace
 {
-    class Inc16BitsTestFixture: public InstructionTestFixture, public ::testing::WithParamInterface<int> {};
+    class Inc16BitsTestFixture: public InstructionTestFixture, public ::testing::WithParamInterface<uint16_t> {};
 
     TEST_P(Inc16BitsTestFixture, INC16Bits)
     {
@@ -37,5 +43,29 @@ namespace
         EXPECT_EQ(8, cycle);
     }
 
-    INSTANTIATE_TEST_CASE_P(Inc16BitsTest, Inc16BitsTestFixture, ::testing::Values(0, 1, 2, 3));
+    INSTANTIATE_TEST_SUITE_P(
+        Inc16BitsTest, Inc16BitsTestFixture, REGISTER_16_BITS_VALUES, NameMapPrinter(REGISTER_16_BITS_VALUE_NAME_MAP)
+    );
+
+    class Inc8BitsTestFixture: public InstructionTestFixture, public ::testing::WithParamInterface<uint16_t> {};
+
+    TEST_P(Inc8BitsTestFixture, INC8Bits)
+    {
+        uint16_t register_index = GetParam();
+        uint16_t instruction_index = (register_index << 3) + 0b100;
+        // Test from Chapter 4: page 107
+        REGISTER_8_BITS_VALUE_SETTER_MAP.at(register_index)(registers, 0xA8);
+        registers.F = 0;
+        REGISTER_8_BITS_VALUE_SETTER_MAP.at(register_index)(expected_registers, 0xA9);
+        expected_registers.F = 0;
+        set_expected_pc_increase(1);
+
+        const auto cycle = gen::INSTRUCTION_FUNCTIONS[instruction_index](arguments, registers, controller);
+
+        EXPECT_EQ(4, cycle);
+    }
+
+    INSTANTIATE_TEST_SUITE_P(
+        Inc8BitsTest, Inc8BitsTestFixture, REGISTER_8_BITS_VALUES, NameMapPrinter(REGISTER_8_BITS_VALUE_NAME_MAP)
+    );
 }
