7d91a64e8cd91a6dac2ce6f9a9b1d84dea536dc6
sylvaus <pierreyves.breches74@gmail.com>
2021-03-13 20:58:09 -0500

    Added RES instruction tests

--- tests/instructions/test_bit_operations.cpp
+++ tests/instructions/test_bit_operations.cpp
@@ -78,6 +78,23 @@ namespace
         EXPECT_EQ(8, cycle);
     }
 
+    TEST_P(BitOpTestFixture, RES)
+    {
+        // Test from Chapter 4: page 114
+        auto [register_index, bit_index] = GetParam();
+        uint16_t instruction_index = register_index + (bit_index << 3) + 0b1'1000'0000;
+        REGISTER_8_BITS_VALUE_SETTER_MAP.at(register_index)(registers, 0xFF);
+        registers.F = emulator::memory::make_flag(true, true, true, true);
+        REGISTER_8_BITS_VALUE_SETTER_MAP.at(register_index)(expected_registers, 0xFF - (1 << bit_index));
+        expected_registers.F = emulator::memory::make_flag(true, true, true, true);
+
+        set_expected_pc_increase(2);
+
+        const auto cycle = gen::INSTRUCTION_FUNCTIONS[instruction_index](arguments, registers, controller);
+
+        EXPECT_EQ(8, cycle);
+    }
+
     INSTANTIATE_TEST_SUITE_P(
         BitOpTest, BitOpTestFixture,
         ::testing::Combine(REGISTER_8_BITS_VALUES, BIT_INDEXES), NamePrinter(REGISTER_8_BITS_VALUE_NAME_MAP)
@@ -119,6 +136,25 @@ namespace
         const auto cycle = gen::INSTRUCTION_FUNCTIONS[instruction_index](arguments, registers, controller);
 
         EXPECT_EQ(12, cycle);
+    }
+
+    TEST_P(BitOpAddressTestFixture, RES)
+    {
+        // Test from Chapter 4: page 114
+        uint16_t bit_index = GetParam();
+        uint16_t instruction_index =  (bit_index << 3) + 0b1'1000'0110;
+        registers.set_HL(0xD1C7);
+        EXPECT_CALL (controller, get(0xD1C7)).Times(1).WillOnce(::testing::Return(0xFF));
+        registers.F = emulator::memory::make_flag(true, true, true, true);
+        expected_registers.set_HL(0xD1C7);
+        EXPECT_CALL (controller, set(0xD1C7, 0xFF - (1 << bit_index))).Times(1);
+        expected_registers.F = emulator::memory::make_flag(true, true, true, true);
+
+        set_expected_pc_increase(2);
+
+        const auto cycle = gen::INSTRUCTION_FUNCTIONS[instruction_index](arguments, registers, controller);
+
+        EXPECT_EQ(16, cycle);
     }
 
     INSTANTIATE_TEST_SUITE_P(BitOpAddressTest, BitOpAddressTestFixture, BIT_INDEXES);
