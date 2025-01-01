421e49a7093cf5311bff41589dcc07ea81db5f74
Sylvaus <sylvaus@users.noreply.github.com>
2025-01-01 16:32:41 -0500

    Implement CGBRegisters behavior for DMG

--- emulator/src/memory/cgb.rs
+++ emulator/src/memory/cgb.rs
@@ -4,26 +4,51 @@ pub const KEY_1_ADDRESS: u16 = 0xFF4D;
 /// Information from: https://gbdev.io/pandocs/CGB_Registers.html#ff56--rp-cgb-mode-only-infrared-communications-port
 pub const INFRARED_CONTROL_ADDRESS: u16 = 0xFF56;
 
-#[derive(Default, Debug)]
-pub struct CGBRegisters {
-    key_1: u8,
-    infrared_control: u8,
+pub enum CGBRegisters {
+    CGB(CGB),
+    DMG,
 }
 
 impl CGBRegisters {
+    pub fn new(is_cgb: bool) -> Self {
+        if is_cgb {
+            Self::CGB(CGB::default())
+        } else {
+            Self::DMG
+        }
+    }
+
     pub fn read_key_1(&self) -> u8 {
-        self.key_1
+        match self {
+            Self::CGB(cgb) => cgb.key_1,
+            Self::DMG => 0xFF,
+        }
     }
 
     pub fn write_key_1(&mut self, value: u8) {
-        self.key_1 = value;
+        match self {
+            Self::CGB(cgb) => cgb.key_1 = value,
+            Self::DMG => (),
+        }
     }
 
     pub fn read_infrared_control(&self) -> u8 {
-        self.infrared_control
+        match self {
+            Self::CGB(cgb) => cgb.infrared_control,
+            Self::DMG => 0xFF,
+        }
     }
 
     pub fn write_infrared_control(&mut self, value: u8) {
-        self.infrared_control = value;
+        match self {
+            Self::CGB(cgb) => cgb.infrared_control = value,
+            Self::DMG => (),
+        }
     }
+}
+
+#[derive(Default, Debug)]
+pub struct CGB {
+    key_1: u8,
+    infrared_control: u8,
 }
